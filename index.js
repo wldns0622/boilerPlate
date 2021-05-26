be5d8aa76e3f2500c813646918adad31a9aa1f6f
wldns0622 <wldns12378@gmail.com>
2021-05-27 01:43:49 +0900

    auth 기능

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const { User } = require('./models/User');
 const config = require('./config/key');
+const { auth } = require('./middleware/auth');
+const { User } = require('./models/User');
 
 // application/x-www-form-urlencoded를 분석
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,7 +29,7 @@ app.get('/', (req, res) => {
   res.send('Connected Server');
 });
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
   // 회원가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
   const user = new User(req.body);
 
@@ -42,7 +43,7 @@ app.post('/register', (req, res) => {
   });
 });
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
   // 요청된 이메일(id)를 데이터베이스에서 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -73,6 +74,20 @@ app.post('/login', (req, res) => {
           .json({ loaginSuccess: true, userId: user._id });
       });
     });
+  });
+});
+
+app.get('/api/users/auth', auth, (req, res) => {
+  // 여기까지 왔다는 것은 미들웨어를 성공적으로 통과 헀다는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
   });
 });
 
