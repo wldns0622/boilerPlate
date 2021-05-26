2031fafc02ea299d2c7ddf198ea8d136a2582212
wldns0622 <wldns12378@gmail.com>
2021-05-27 00:21:15 +0900

    비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+// salt가 몇글자인지 나타냄 -> 10자리인 암호화 salt를 생성함
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -12,7 +15,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    minlength: 50,
+    minlength: 5,
   },
   lastname: {
     type: String,
@@ -31,6 +34,25 @@ const userSchema = mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+// save가 일어나기전에 콜백을 실핸한다.
+userSchema.pre('save', function (next) {
+  // user는 현재 입력된 user
+  var user = this;
+
+  if (user.isModified('password')) {
+    // save가 일어나기전에 여기서 비밀번호를 암호화 시킨 후 보낸다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+    // next();
+  }
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
