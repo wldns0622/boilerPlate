20dc40d98166096b6f9ad3e522ff9e701805fe89
wldns0622 <wldns12378@gmail.com>
2021-06-05 20:55:37 +0900

    login - make login page state

--- client/src/components/views/LoginPage/LoginPage.jsx
+++ client/src/components/views/LoginPage/LoginPage.jsx
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function LoginPage(props) {
-  return <div>Login Page</div>;
+  // 상태
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // 핸들러 함수
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <form
+        style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={onSubmitHandler}
+      >
+        <label htmlFor='email'>Email</label>
+        <input
+          type='email'
+          name='email'
+          id='email'
+          value={email}
+          onChange={onEmailHandler}
+        />
+        <label htmlFor='password'>Password</label>
+        <input
+          type='password'
+          name='password'
+          id='password'
+          value={password}
+          onChange={onPasswordHandler}
+        />
+        <br />
+        <button type='submit'> Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
