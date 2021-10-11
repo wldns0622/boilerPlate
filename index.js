e324e42e33da8dcb7d773997637604dc1f92f713
wldns0622 <wldns12378@gmail.com>
2021-10-11 21:36:56 +0900

    feat: register router

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
+// module
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+// model
+const { User } =require("./models/User");
 
 const app = express();
 const port = 5000;
 
-mongoose.connect(`mongodb+srv://daramjwi:1q2w3e4r!1@boilerplate.itvuc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
-  // useNewUrlParse: true,
-  // useUnifiedTopology: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false
-})
+// application/x-ww-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// pplication/json
+app.use(bodyParser.json());
+
+mongoose.connect(`mongodb+srv://daramjwi:1q2w3e4r!1@boilerplate.itvuc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
 .then(() => console.log('MongoDB Connected...'))
 .catch((err) => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello, World!'));
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+  
+  user.save((err, doc) => {
+    if(err) return res.json({success: false, err});
+    return res.status(200).json({success: true});
+  });
+})
 
 app.listen(port, () => console.log(`Server Conneted... Port: ${port}`));
