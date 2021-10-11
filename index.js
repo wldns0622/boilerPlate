799e01fcd622da946863b1dd858b97ce109a2cf2
wldns0622 <wldns12378@gmail.com>
2021-10-11 21:41:03 +0900

    chore: apply nodemon

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ mongoose.connect(`mongodb+srv://daramjwi:1q2w3e4r!1@boilerplate.itvuc.mongodb.ne
 .then(() => console.log('MongoDB Connected...'))
 .catch((err) => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello, World!'));
+app.get('/', (req, res) => res.send('wow!'));
 
 app.post('/register', (req, res) => {
   const user = new User(req.body);
-  
+
   user.save((err, doc) => {
     if(err) return res.json({success: false, err});
     return res.status(200).json({success: true});
